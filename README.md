005336d845b47b363c1f0e6616286814ac735568
kas2020-commits <kas2020@protonmail.com>
2020-09-18 15:44:05 -0400

    added readme; added pub use

---
+++ README.md
@@ -0,0 +1,4 @@
+## raxolotl
+
+Raxolotl is an adaptation from the origional
+[Axolotl++](https://github.com/kas2020-commits/axolotl) library.
--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,31 @@
-#![cfg_attr(test, allow(dead_code))]
-#![warn(
-    missing_docs,
-    missing_debug_implementations,
-    missing_copy_implementations,
-    trivial_casts,
-    trivial_numeric_casts,
-    unsafe_code,
-    unstable_features,
-    unused_import_braces,
-    unused_qualifications
-)]
+// #![cfg_attr(test, allow(dead_code))]
+// #![warn(
+//     missing_docs,
+//     missing_debug_implementations,
+//     missing_copy_implementations,
+//     trivial_casts,
+//     trivial_numeric_casts,
+//     unsafe_code,
+//     unstable_features,
+//     unused_import_braces,
+//     unused_qualifications
+// )]
 
 mod findroot;
 mod integrals;
 mod matrix;
 mod ode;
+
+pub use findroot::{
+    newton_raphson, bisection, secent,
+};
+
+pub use integrals::{
+    composite_midpoint, composite_trapezoid, composite_simpson,
+};
+
+pub use ode::{
+    euler, trapezoidal
+};
+
+pub use matrix::Matrix;
